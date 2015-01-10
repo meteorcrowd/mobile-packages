1240adafd71dafe320137891fd6cfa2f1aaabccc
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-10 19:52:54 +0200

    Inline documentation.

--- server/methods.js
+++ server/methods.js
@@ -1,21 +1,28 @@
 Meteor.methods({
     geocodeReverse: function(latlng) {
-        console.log('geocode_reverse');
+        // This funciton returns details about a location
+        // such as city, country, street, etc.
+        // Accepts latitude and longitude as input
         try {
+            // Create a GeoCoder
             var geo = new GeoCoder({
                 geocoderProvider: "openstreetmap",
                 httpAdapter: "http"
             });
+
+            // Get the reverse geolocation, based on latitude and longitude
             var result = geo.reverse(latlng.lat, latlng.lng);
-            console.log("Geocoding result: ", result);
+
+            // Return the geocoded details
             return result[0];
         }
         catch (e){return "sea";}
     },
     addStory: function( story ) {
-
+        // add reverse geolocation to story. E.g. city, country, etc.
         story.location = Meteor.call( "geocodeReverse", story.location );
-        console.log("Story city: ", story.location.city);
-        Stories.insert( story) ;
+
+        // Save the story into the stories collection
+        Stories.insert( story ) ;
     }
 });
