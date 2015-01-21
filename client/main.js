7d2c8f691e5966dd7c659d250bb139cbd9d4f382
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-21 18:12:11 +0200

    Removed Geolocation initialization.

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
 // https://github.com/meteor/mobile-packages/tree/master/packages/mdg:geolocation
 // A reactive var needs to be instantiated first.
 // We call the function when the app loads
-Geolocation.latLng();
+//Geolocation.latLng();
