9fa81fbf556d0ffa14a33880f7b429c7fd7060b7
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-21 17:57:55 +0200

    Nearby and newest stories publications.

--- server/publications.js
+++ server/publications.js
@@ -1,12 +1,23 @@
-Meteor.publish('nearbyStories', function(location, minDistance, maxDistance) {
+Meteor.publish('nearbyStories', function(coordinates, minDistance, maxDistance) {
+    /*
+    * Takes an array of coordinates in longitude/latitude format
+    * min/max distance expressed in meters
+    * returns nearest stories
+    */
     var parameters;
     parameters = {
         geometry: {
             type: "Point" ,
-            coordinates: location.coordinates
+            // array of longitude/latitude
+            coordinates: coordinates
         },
         $maxDistance: maxDistance,
         $minDistance: minDistance
     }
-    stories = Stories.find({ loc : {$near: parameters } });
+    return  Stories.find({ loc : {$near: parameters } });
+});
+
+Meteor.publish('newestStories', function () {
+    // Get the most recent stories.
+    return Stories.find({}, {sort: {"createdAt": -1}});
 });
